d50726ef14e6593ebba780a34e8ba984bbde4bb3
Chanatinart K <chanatinart.k@gmail.com>
2023-03-13 16:21:30 +0700

    complete

---
+++ javascript.js
@@ -0,0 +1,23 @@
+function add(a, b){
+    return a + b
+}
+
+function subtract(a, b){
+    return a - b
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(a, b){
+    return a / b
+}
+
+// console.log(divide(10, 5))
+
+function info(firstName, location, hobby) {
+    console.log(`Hi, my name is ${firstName} I live in  ${location} and enjoy ${hobby}`)
+}
+
+info('Chanatinart', 'Bangkok', 'play games')
